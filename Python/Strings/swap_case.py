57d1dc0182c0bab923ad9c7a3533f77b3cbe936b
sofiane boumelit <sofianeboumelit@protonmail.ch>
2026-06-18 20:25:57 +0100

    reorganize folder

---
+++ Python/Strings/swap_case.py
@@ -0,0 +1,51 @@
+"""
+You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+
+For Example:
+
+Www.HackerRank.com → wWW.hACKERrANK.COM
+Pythonist 2 → pYTHONIST 2  
+
+Function Description
+
+Complete the swap_case function in the editor below.
+
+swap_case has the following parameters:
+
+    string s: the string to modify
+
+Returns
+
+    string: the modified string
+
+Input Format
+
+A single line containing a string
+
+.
+
+Constraints
+
+Sample Input 0
+
+HackerRank.com presents "Pythonist 2".
+
+Sample Output 0
+
+hACKERrANK.COM PRESENTS "pYTHONIST 2".
+
+"""
+
+def swap_case(s):
+    r=""
+    for item in s:
+        if item.islower(): r = r + item.upper()
+        else : r = r + item.lower()   
+    return r
+
+'''if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+'''
+print(swap_case('Python 2'))
